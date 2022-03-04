49d7ba0258f5b62fa974d16c979043416ed59b63
Rahul <rahul.k@simformsolutions.com>
2022-03-04 18:28:07 +0530

    Layouts & rendering bugs fixed

--- config/routes.rb
+++ config/routes.rb
@@ -4,18 +4,7 @@ Rails.application.routes.draw do
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
-  
-  # resources :users
-  # get 'orders/index'
-  # get 'orders/show'
-  # get 'orders/edit'
-  # get 'orders/new'
-  # get 'products/index'
-  # get 'products/show'
-  # get 'products/new'
-  # get 'products/edit'
   root to: "homes#index"
-
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   namespace :admin do
     resources :products do 
@@ -23,8 +12,4 @@ Rails.application.routes.draw do
     end
   end
   resources :products
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-
 end
